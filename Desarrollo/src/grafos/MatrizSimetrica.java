e1d47b09197f558919393fd027d5ea3007e85606
Alvain <alvain93@gmail.com>
2019-11-06 18:05:25 -0300

    Creo clase MatrizSimetrica

---
+++ Desarrollo/src/grafos/MatrizSimetrica.java
@@ -0,0 +1,40 @@
+package grafos;
+
+public class MatrizSimetrica {
+
+	private int dimensiones;
+	private int[] matriz;
+	
+	public MatrizSimetrica(int dimensiones){
+		this.dimensiones = dimensiones;
+		//vector con el tamaño para uno de los triangulos y la diagonal principal
+		matriz = new int[((dimensiones+1)*dimensiones)/2];
+	}
+	
+	public boolean agregarNumero(int fila,int columna,int num) {
+		if(fila>=dimensiones || columna>=dimensiones)
+			return false;
+		int filaAux,columnaAux;
+		if(fila>columna) {
+			filaAux = columna;
+			columnaAux = fila;
+		}else {
+			filaAux = fila;
+			columnaAux = columna;
+		}
+		matriz[(((dimensiones+1)*dimensiones)/2)-(((dimensiones-filaAux+1)*(dimensiones-filaAux))/2)+(columnaAux-filaAux)] = num;
+		return true;
+	}
+	
+	public int getNumero(int fila,int columna) {
+		int filaAux,columnaAux;
+		if(fila>columna) {
+			filaAux = columna;
+			columnaAux = fila;
+		}else {
+			filaAux = fila;
+			columnaAux = columna;
+		}
+		return matriz[(((dimensiones+1)*dimensiones)/2)-(((dimensiones-filaAux+1)*(dimensiones-filaAux))/2)+(columnaAux-filaAux)];
+	}
+}
