4e165ada84bbb0f846fc18229436554fb8ba6b29
fedeecala <fedee.cala@gmail.com>
2019-11-13 15:11:47 -0300

    Agregue Programa Probador y termine los regulares
    Agregue Programa Probador y termine los regulares

--- Desarrollo/src/grafo/GeneradorDeGrafos.java
+++ Desarrollo/src/grafo/GeneradorDeGrafos.java
@@ -67,6 +67,104 @@ public class GeneradorDeGrafos {
 		return grafo;
 	}
 
+	// -------------------------------REGULARES-------------------------------------------------------
+		// Genera grafo regular dadas cantidad N de nodos y grado
+		public static GrafoNDNP generarRegularNYGrado(int cantNodos, int grado) {
+			MatrizSimetrica matriz;
+			int cantidadAristas = 0;
+			double porcentajeDeAdyacencia;
+			int saltoMax, j;
+
+			if (grado >= cantNodos) {
+				System.out.println("No se puede generar el grafo.");
+				return null;
+			}
+
+			matriz = new MatrizSimetrica(cantNodos);
+
+			if (((cantNodos % 2) == 0) || ((grado % 2) == 0)) {
+				saltoMax = (grado) / 2;
+
+				for (int salto = 0; salto <= saltoMax; salto++) {
+					for (int i = 1; i < cantNodos; i++) {
+						j = (i + salto) % cantNodos;
+
+						matriz.setIJ(i, j);
+						cantidadAristas++;
+					}
+				}
+
+				if (grado % 2 != 0) {
+					for (int i = 0; i < cantNodos / 2; i++) {
+						j = (i + cantNodos) / 2;
+						matriz.setIJ(i, j);
+						cantidadAristas++;
+					}
+				}
+			}
+
+			porcentajeDeAdyacencia = (grado * (cantNodos - 1)) * 100;
+
+			GrafoNDNP grafo = new GrafoNDNP(matriz, cantNodos, cantidadAristas, porcentajeDeAdyacencia, grado, grado);
+			return grafo;
+		}
+
+		// Genera grafo regular dadas cantidad N de nodos y porcentaje de adyacencia
+		public static GrafoNDNP generarRegularNYAdyacencia(int cantNodos, double porcAdyacencia) {
+			MatrizSimetrica matriz;
+			int grado = (int) (((porcAdyacencia / 100) * (cantNodos - 1)));
+			int cantAristas = (cantNodos * grado) / 2;
+			int saltoMax, j;
+
+			matriz = new MatrizSimetrica(cantNodos);
+
+			if (((cantNodos % 2) == 0) || ((grado % 2) == 0)) {
+				saltoMax = (grado) / 2;
+
+				for (int salto = 0; salto <= saltoMax; salto++) {
+					for (int i = 1; i < cantNodos; i++) {
+						j = (i + salto) % cantNodos;
+
+						matriz.setIJ(i, j);
+					}
+				}
+
+				if (grado % 2 != 0) {
+					for (int i = 0; i < cantNodos / 2; i++) {
+						j = (i + cantNodos) / 2;
+						matriz.setIJ(i, j);
+					}
+				}
+			}
+
+			GrafoNDNP grafo = new GrafoNDNP(matriz, cantNodos, cantAristas, porcAdyacencia, grado, grado);
+			return grafo;
+		}
+
+		// -------------------------------nPartito-------------------------------------------------------
+		// Genera grafo n-Partito dadas cantidad N de nodos y cantidad n de grupos
+		public static GrafoNDNP generarGrafoNPartito(int cantNodos, int n) {
+			MatrizSimetrica matriz;
+			if (cantNodos % n != 0 && n >= cantNodos) {
+				System.out.println("No se puede generar grafo n partito.");
+				return null;
+			}
+
+			int cantAristas = 0;
+
+			matriz = new MatrizSimetrica(cantNodos);
+
+			for (int i = 0; i < cantNodos; i += 2) {
+				for (int j = i + 2; j < cantNodos; j++) {
+					matriz.setIJ(i, j);
+					cantAristas++;
+				}
+			}
+			double porcAdyacencia = (2 * (double) cantAristas * 100) / ((cantNodos * (cantNodos - 1)) / 2);
+			calcularGradoMinYMax(matriz, cantNodos);
+			GrafoNDNP grafo = new GrafoNDNP(matriz, cantNodos, cantAristas, porcAdyacencia, gradoMax, gradoMax);
+			return grafo;
+		}
 
 	
 	private static void calcularGradoMinYMax(MatrizSimetrica matriz, int cantNodos) {
--- Desarrollo/src/grafo/GrafoNDNP.java
+++ Desarrollo/src/grafo/GrafoNDNP.java
@@ -1,5 +1,10 @@
 package grafo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class GrafoNDNP {
 	private MatrizSimetrica matriz;
 	private int cantidadDeNodos;
@@ -10,6 +15,56 @@ public class GrafoNDNP {
 	private Nodo[] nodos;
 	private int cantidadDeColores; 
 	
+	public GrafoNDNP(String path) {
+		BufferedReader bf = null;
+		FileReader entrada = null;
+		String linea;
+		String[] data;
+		boolean bandera = false;
+		int fila, columna;
+
+		try {
+			entrada = new FileReader(new File(path));
+			bf = new BufferedReader(entrada);
+
+			while ((linea = bf.readLine()) != null) {
+				data = linea.split(" ");
+				if (!bandera) {
+					this.cantidadDeNodos = Integer.parseInt(data[0]);
+					this.cantidadDeAristas = Integer.parseInt(data[1]);
+					this.porcentajeAdyacencia = Double.parseDouble(data[2]);
+					this.gradoMinimo = Integer.parseInt(data[3]);
+					this.gradoMaximo = Integer.parseInt(data[4]);
+
+					this.nodos = new Nodo[this.cantidadDeNodos];
+
+					for (int i = 0; i < this.cantidadDeNodos; i++) {
+						this.nodos[i] = new Nodo(i, 0, 0);
+					}
+
+					this.matriz = new MatrizSimetrica(this.cantidadDeNodos);
+					bandera = true;
+				} else {
+					fila = Integer.parseInt(data[0]);
+					columna = Integer.parseInt(data[1]);
+
+					matriz.setIJ(fila, columna);
+					this.nodos[fila].setGrado(this.nodos[fila].getGrado() + 1);
+					this.nodos[columna].setGrado(this.nodos[columna].getGrado() + 1);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bf.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	public GrafoNDNP(MatrizSimetrica matriz, int cantNodos, int cantAristas, double PorcAdyacencia, int gMin, int gMax) {
 		this.matriz = matriz;
 		this.cantidadDeNodos = cantNodos;
@@ -18,5 +73,38 @@ public class GrafoNDNP {
 		this.gradoMinimo = gMin;
 		this.gradoMaximo = gMax;
 	}
+	
+	public MatrizSimetrica getMatrizSimetrica() {
+		return this.matriz;
+	}
 
+	public void colorearPowell() {
+		ordenarGradoMayorAMenor(nodos, 0, nodos.length - 1);
+		colorear();
+	}
+
+	private void ordenarGradoMayorAMenor(Nodo[] nodos2, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void colorearMatula() {
+
+		ordenarGradoMenorAMayor(nodos, 0, nodos.length - 1);
+		colorear();
+	}
+
+	private void ordenarGradoMenorAMayor(Nodo[] nodos2, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void colorearSecuencialAleatorio() {
+		colorear();
+	}
+
+	private void colorear() {
+	}
 }
+
+
